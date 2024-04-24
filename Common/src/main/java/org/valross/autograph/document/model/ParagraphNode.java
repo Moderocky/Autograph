f59645b298a00903acc686c847a4c5ba395e7a20
Moderocky <admin@moderocky.com>
2024-04-24 10:19:25 +0100

    Add simple text delegator.

--- Common/src/main/java/org/valross/autograph/document/model/ParagraphNode.java
+++ Common/src/main/java/org/valross/autograph/document/model/ParagraphNode.java
@@ -5,9 +5,14 @@ import mx.kenzie.hypertext.element.StandardElements;
 import org.valross.autograph.document.ModelNode;
 import org.valross.autograph.document.MultiNode;
 import org.valross.autograph.document.Node;
+import org.valross.autograph.document.TextNode;
 import org.valross.constantine.RecordConstant;
 
 public record ParagraphNode(Node... nodes) implements MultiNode, ModelNode, RecordConstant {
+
+    public ParagraphNode(String text) {
+        this(new TextNode(text));
+    }
 
     @Override
     public HTMElement compile() {
