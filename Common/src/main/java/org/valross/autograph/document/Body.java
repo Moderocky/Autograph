db5e0fbc882dd349adbed324a3e63ef75dfbf03e
Moderocky <admin@moderocky.com>
2024-04-26 11:50:24 +0100

    Allow body to be text-simplified.

--- Common/src/main/java/org/valross/autograph/document/Body.java
+++ Common/src/main/java/org/valross/autograph/document/Body.java
@@ -1,5 +1,7 @@
 package org.valross.autograph.document;
 
+import org.valross.autograph.document.model.ParagraphNode;
+import org.valross.autograph.error.AutographException;
 import org.valross.constantine.RecordConstant;
 
 import java.io.IOException;
@@ -16,6 +18,31 @@ public record Body(Node... nodes) implements RecordConstant, MultiNode {
     @Override
     public void write(OutputStream stream, Charset charset) throws IOException {
         for (Node node : nodes) node.write(stream, charset);
+    }
+
+    public boolean isText() {
+        if (nodes.length != 1) return false;
+        if (nodes[0] instanceof TextNode) return true;
+        return nodes[0] instanceof ParagraphNode node
+            && node.nodes().length == 1
+            && node.nodes()[0] instanceof TextNode;
+    }
+
+    public TextNode asText() {
+        if (nodes[0] instanceof TextNode node) return node;
+        else if (nodes[0] instanceof ParagraphNode node)
+            return (TextNode) node.nodes()[0];
+        throw new AutographException("Not a text");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return nodes.length == 0 || (nodes.length == 1 && nodes[0].isEmpty());
+    }
+
+    public boolean isBlank() {
+        return nodes.length == 0
+            || (nodes.length == 1 && nodes[0] instanceof TextNode node && node.value().isBlank());
     }
 
 }
